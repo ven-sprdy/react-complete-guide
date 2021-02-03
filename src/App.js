27675d189d322dd0db169bb5651345a8fff88a49
ven-sprdy <ven.sprdy@gmail.com>
2021-02-02 23:46:02 -0600

    setState udemy section 3 video 42

--- src/App.js
+++ src/App.js
@@ -6,16 +6,30 @@ class App extends Component {
 
   state = {
     persons: [
-      {name: "Prasad", age: 34},
-      {name: "Ramya", age: 29},
-      {name: "Prayan", age: 4}
+      { name: "Prasad", age: 34 },
+      { name: "Ramya", age: 29 },
+      { name: "Prayan", age: 4 }
     ]
+  }
+
+  switchNameHandler = () => {
+    this.setState( {
+      persons: [
+        { name: "Prasad", age: 35 },
+        { name: "Ramya", age: 29 },
+        { name: "Prayan", age: 4 }
+      ]
+    } )
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.persons.map((person, index) => <Person key={index} name={person.name} age={person.age} />)}
+        <br />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {this.state.persons.map(
+          (person, index) => <Person key={index} name={person.name} age={person.age} />
+        )}
       </div>
     );
   }
--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -1,47 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Person extends Component { 
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasError: false,
-    };
-  }
-
-  // componentWillMount = () => {
-  //   console.log('Person will mount');
-  // }
-
-  // componentDidMount = () => {
-  //   console.log('Person mounted');
-  // }
-
-  // componentWillReceiveProps = (nextProps) => {
-  //   console.log('Person will receive props', nextProps);
-  // }
-
-  // componentWillUpdate = (nextProps, nextState) => {
-  //   console.log('Person will update', nextProps, nextState);
-  // }
-
-  // componentDidUpdate = () => {
-  //   console.log('Person did update');
-  // }
-
-  // componentWillUnmount = () => {
-  //   console.log('Person will unmount');
-  // }
-
-  render () {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
-    }
-    return (
-      <div className="PersonWrapper">
-        <p>I'm {this.props.name} and I'm {this.props.age} years old.</p>
+const person = (props) => {
+  return (
+    <div className="PersonWrapper">
+        <p>I'm {props.name} and I'm {props.age} years old.</p>
       </div>
-    );
-  }
+  );
 }
 
-export default Person;
+export default person;
